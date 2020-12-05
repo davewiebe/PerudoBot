e749fb4ed553c3f60d98a672bbcc89f0f3b9d5b3
Dave Wiebe <dawiebe@gmail.com>
2020-12-05 09:05:58 -0800

    Refactoring services

---
+++ PerudoBot/Modules/ReactionCommands.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using PerudoBot.Database.Sqlite.Data;
+
+namespace PerudoBot.Modules
+{
+    public partial class ReactionCommands : ModuleBase<CommandContext>
+    {
+        private readonly PerudoBotDbContext _db;
+
+        public ReactionCommands()
+        {
+            //TODO: Let DI handle instantiation
+            _db = new PerudoBotDbContext();
+        }
+    }
+}
---
+++ PerudoBot/Services/DiscordServices.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerudoBot.Services
+{
+    public class DiscordServices : ModuleBase<SocketCommandContext>
+    {
+        private SocketCommandContext _context;
+
+        public DiscordServices(SocketCommandContext context)
+        {
+            _context = context;
+        }
+
+        private async Task<IUserMessage> SendMessageAsync(string message, bool isTTS = false)
+        {
+            if (string.IsNullOrEmpty(message)) return null;
+
+            var requestOptions = new RequestOptions()
+            { RetryMode = RetryMode.RetryRatelimit };
+            return await base.ReplyAsync(message, options: requestOptions, isTTS: isTTS);
+        }
+
+        private async Task SendTempMessageAsync(string message, bool isTTS = false)
+        {
+            var requestOptions = new RequestOptions()
+            { RetryMode = RetryMode.RetryRatelimit };
+            var sentMessage = await base.ReplyAsync(message, options: requestOptions, isTTS: isTTS);
+            try
+            {
+                _ = sentMessage.DeleteAsync();
+            }
+            catch
+            { }
+        }
+
+        private void DeleteCommandFromDiscord(ulong? messageId = null)
+        {
+            try
+            {
+                if (messageId != null)
+                {
+                    _ = Task.Run(() => _context.Channel.DeleteMessageAsync(messageId.Value));
+                }
+                else
+                {
+                    _ = Task.Run(() => _context.Message.DeleteAsync());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
