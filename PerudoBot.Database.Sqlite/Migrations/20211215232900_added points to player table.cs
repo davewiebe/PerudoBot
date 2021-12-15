b4a90a157007b853fec80a361a9e3bcc87fe76ae
Josh Shercliffe <joshuashercliffe@gmail.com>
2021-12-15 15:29:21 -0800

    added migration

---
+++ PerudoBot.Database.Sqlite/Migrations/20211215232900_added points to player table.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PerudoBot.Database.Sqlite.Migrations
+{
+    public partial class addedpointstoplayertable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Points",
+                table: "Players",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Points",
+                table: "Players");
+        }
+    }
+}
