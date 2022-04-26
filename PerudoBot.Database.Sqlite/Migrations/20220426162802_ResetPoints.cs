2fcd8af779753891640aba0273cd27b64eabc9c8
agoryelov <andrey.goryelov@gmail.com>
2022-04-26 09:38:40 -0700

    Reset all points to 200

---
+++ PerudoBot.Database.Sqlite/Migrations/20220426162802_ResetPoints.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PerudoBot.Database.Sqlite.Migrations
+{
+    public partial class ResetPoints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE [Players] SET [UsedPoints] = 0");
+            migrationBuilder.Sql("UPDATE [Players] SET [TotalPoints] = 200");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
