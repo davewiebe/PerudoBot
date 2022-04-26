29d873ba651683f1aec8861cd1eabe5ea879f9e2
agoryelov <andrey.goryelov@gmail.com>
2022-04-26 00:27:52 -0700

    Adjusted prices and limited uses per game to 1

--- PerudoBot.GameService/Constants/PowerUp.cs
+++ PerudoBot.GameService/Constants/PowerUp.cs
@@ -15,7 +15,7 @@ namespace PerudoBot.GameService.Constants
         public int MinPlayers { get; set; } = 3;
         public int MinDice { get; set; } = 1;
         public int UsesPerRound { get; set; } = 1;
-        public int UsesPerGame { get; set; } = 100;
+        public int UsesPerGame { get; set; } = 1;
     }
 
     public static class PowerUps
@@ -25,29 +25,29 @@ namespace PerudoBot.GameService.Constants
         public static PowerUp Lifetap = new PowerUp
         {
             Name = "Lifetap",
-            Description = "Permanently lose a life to get dice",
+            Description = "Permanently lose a life to get dice this turn",
+            UsesPerGame = 5
         };
 
         public static PowerUp Touch = new PowerUp
         {
             Name = "Touch",
             Description = "If you're going off the grid, might want to check the temperature first",
-            Cost = 25
+            Cost = 20
         };
 
         public static PowerUp Gamble = new PowerUp
         {
             Name = "Gamble",
             Description = "Transform your dice unpredictably, use `!odds` to find out more",
-            Cost = 50,
-            MinDice = 2,
+            Cost = 40
         };
 
         public static PowerUp Steal = new PowerUp
         {
             Name = "Steal",
             Description = "Steal 2-3 dice from a target player, new dice are mystery",
-            Cost = 100,
+            Cost = 60
         };
 
         public static List<PowerUp> PowerUpList = new List<PowerUp>
